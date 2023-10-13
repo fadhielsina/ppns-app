7b8ea60d42d14e3e6b26f2c171b26743d7300b69
Fadhiel Ibnu Sina <fadhielsina@gmail.com>
2023-10-14 04:42:30 +0700

    update url

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -17,17 +17,17 @@
     <meta name="keywords" content="Admin , Responsive, Landing, Bootstrap, App, Template, Mobile, iOS, Android, apple, creative app">
     <meta name="author" content="#">
     <!-- Favicon icon -->
-    <link rel="icon" href="..\files\assets\images\favicon.ico" type="image/x-icon">
+    <link rel="icon" href="{{url('')}}\files\assets\images\favicon.ico" type="image/x-icon">
     <!-- Google font-->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,800" rel="stylesheet">
     <!-- Required Fremwork -->
-    <link rel="stylesheet" type="text/css" href="..\files\bower_components\bootstrap\css\bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="{{url('')}}\files\bower_components\bootstrap\css\bootstrap.min.css">
     <!-- themify-icons line icon -->
-    <link rel="stylesheet" type="text/css" href="..\files\assets\icon\themify-icons\themify-icons.css">
+    <link rel="stylesheet" type="text/css" href="{{url('')}}\files\assets\icon\themify-icons\themify-icons.css">
     <!-- ico font -->
-    <link rel="stylesheet" type="text/css" href="..\files\assets\icon\icofont\css\icofont.css">
+    <link rel="stylesheet" type="text/css" href="{{url('')}}\files\assets\icon\icofont\css\icofont.css">
     <!-- Style.css -->
-    <link rel="stylesheet" type="text/css" href="..\files\assets\css\style.css">
+    <link rel="stylesheet" type="text/css" href="{{url('')}}\files\assets\css\style.css">
 </head>
 
 <body class="fix-menu">
@@ -80,7 +80,7 @@
                     <form class="md-float-material form-material" action="" method="POST">
                         @csrf
                         <div class="text-center">
-                            <img src="..\files\assets\images\logo.png" alt="logo.png">
+                            <img src="{{url('')}}\files\assets\images\logo.png" alt="logo.png">
                         </div>
                         <div class="auth-box card">
                             <div class="card-block">
@@ -179,21 +179,21 @@
 <![endif]-->
     <!-- Warning Section Ends -->
     <!-- Required Jquery -->
-    <script type="text/javascript" src="..\files\bower_components\jquery\js\jquery.min.js"></script>
-    <script type="text/javascript" src="..\files\bower_components\jquery-ui\js\jquery-ui.min.js"></script>
-    <script type="text/javascript" src="..\files\bower_components\popper.js\js\popper.min.js"></script>
-    <script type="text/javascript" src="..\files\bower_components\bootstrap\js\bootstrap.min.js"></script>
+    <script type="text/javascript" src="{{url('')}}\files\bower_components\jquery\js\jquery.min.js"></script>
+    <script type="text/javascript" src="{{url('')}}\files\bower_components\jquery-ui\js\jquery-ui.min.js"></script>
+    <script type="text/javascript" src="{{url('')}}\files\bower_components\popper.js\js\popper.min.js"></script>
+    <script type="text/javascript" src="{{url('')}}\files\bower_components\bootstrap\js\bootstrap.min.js"></script>
     <!-- jquery slimscroll js -->
-    <script type="text/javascript" src="..\files\bower_components\jquery-slimscroll\js\jquery.slimscroll.js"></script>
+    <script type="text/javascript" src="{{url('')}}\files\bower_components\jquery-slimscroll\js\jquery.slimscroll.js"></script>
     <!-- modernizr js -->
-    <script type="text/javascript" src="..\files\bower_components\modernizr\js\modernizr.js"></script>
-    <script type="text/javascript" src="..\files\bower_components\modernizr\js\css-scrollbars.js"></script>
+    <script type="text/javascript" src="{{url('')}}\files\bower_components\modernizr\js\modernizr.js"></script>
+    <script type="text/javascript" src="{{url('')}}\files\bower_components\modernizr\js\css-scrollbars.js"></script>
     <!-- i18next.min.js -->
-    <script type="text/javascript" src="..\files\bower_components\i18next\js\i18next.min.js"></script>
-    <script type="text/javascript" src="..\files\bower_components\i18next-xhr-backend\js\i18nextXHRBackend.min.js"></script>
-    <script type="text/javascript" src="..\files\bower_components\i18next-browser-languagedetector\js\i18nextBrowserLanguageDetector.min.js"></script>
-    <script type="text/javascript" src="..\files\bower_components\jquery-i18next\js\jquery-i18next.min.js"></script>
-    <script type="text/javascript" src="..\files\assets\js\common-pages.js"></script>
+    <script type="text/javascript" src="{{url('')}}\files\bower_components\i18next\js\i18next.min.js"></script>
+    <script type="text/javascript" src="{{url('')}}\files\bower_components\i18next-xhr-backend\js\i18nextXHRBackend.min.js"></script>
+    <script type="text/javascript" src="{{url('')}}\files\bower_components\i18next-browser-languagedetector\js\i18nextBrowserLanguageDetector.min.js"></script>
+    <script type="text/javascript" src="{{url('')}}\files\bower_components\jquery-i18next\js\jquery-i18next.min.js"></script>
+    <script type="text/javascript" src="{{url('')}}\files\assets\js\common-pages.js"></script>
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
     <script>
